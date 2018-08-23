26fad91320270282c644023c7eb6d78bb324640c
99thotslayer <42628004+99thotslayer@users.noreply.github.com>
2018-08-23 11:34:42 +1000

    Initial commit

---
+++ Assets/Intro/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpHeight = 10f;
+    public Rigidbody rigid;
+    public float rayDistance = 1f;
+
+    private bool isGrounded = true;
+
+    private void OnDrawGizmos()
+    {
+        Ray groundRay = new Ray(transform.position, Vector3.down);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(groundRay.origin, groundRay.origin + groundRay.direction * ray)
+
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		bool IsGrounded()
+        {
+            Ray groundRay = new Ray(transform, position
+        }
+	}
+
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float inputH = Input.GetAxis("Horizontal") * moveSpeed;
+        float inputV = Input.GetAxis("Vertical") * moveSpeed;
+        Vector3 moveDir = new Vector3(inputH, 0f, inputV);
+
+        Vector3 force = new Vector3(moveDir.x, rigid.velocity.y, moveDir.z);
+
+        if (Input.GetButton("Jump") && IsGrounded())
+        {
+            force.y = jumpHeight;
+        }
+        rigid.velocity = force * moveSpeed;
+	}
+}
